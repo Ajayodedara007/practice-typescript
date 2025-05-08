4f2821281ef577d1ab4407e17cb4ee4002e9f732
Ajay Odedara <ajayodedara.mks@outlook.com>
2025-05-08 12:57:01 +0530

    Types vs Interfaces

--- src/types/Task.ts
+++ src/types/Task.ts
@@ -1,8 +1,15 @@
 export type TaskStatus = "todo" | "in-progress" | "done";
 
-export type Task = {
+export interface Task {
   id: number;
   title: string;
   description: string;
   completed: TaskStatus;
-};
+}
+
+// export type Task = {
+//   id: number;
+//   title: string;
+//   description: string;
+//   completed: TaskStatus;
+// };
