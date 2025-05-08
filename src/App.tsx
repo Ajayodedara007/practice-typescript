00bc09a3f16135cd871a4bf79b6e793bcc377c40
Ajay Odedara <ajayodedara.mks@outlook.com>
2025-05-08 12:45:43 +0530

    Types & Type Annotations

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import type { Task } from "./types/Task";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const tasks: Task[] = [
+    {
+      id: 1,
+      title: "Buy groceries",
+      description: "Milk, Bread, Eggs",
+      completed: false,
+    },
+    {
+      id: 2,
+      title: "Study TypeScript",
+      description: "Learn types and interfaces",
+      completed: true,
+    },
+  ];
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="p-10 flex items-center justify-center bg-green-200">
+        <h1 className="text-4xl font-bold text-green-700">
+          Tailwind CSS is Working!
+        </h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
